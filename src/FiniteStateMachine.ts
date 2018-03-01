cf2267942d4120e9ea51368e51562c415b21de00
Cristian <caxvalencia@gmail.com>
2018-03-01 02:15:42 -0500

    Refactor for FSM class

--- src/FiniteStateMachine.ts
+++ src/FiniteStateMachine.ts
@@ -1,39 +1,39 @@
-
-
 export class FiniteStateMachine {
-   
     str: string;
     currentState: number;
     states: any;
     accepts: Array<number>;
     isAccepted: boolean;
-    
-    constructor( str: string, currentState: number, states, accepts: Array<number> ) {
+
+    constructor(
+        str: string,
+        currentState: number,
+        states,
+        accepts: Array<number>
+    ) {
         this.str = str;
         this.currentState = currentState;
         this.states = states;
         this.accepts = accepts;
-        this.isAccepted = this.run( this.str );
+        this.isAccepted = this.run(this.str);
     }
 
-    run( string ) {
-        if( string === '' ) {
-            return this.accepts.indexOf( this.currentState ) !== -1;
+    run(string) {
+        if (string === '') {
+            return this.accepts.indexOf(this.currentState) !== -1;
         }
 
-        let character = string[ 0 ];
-        let state = this.states[ this.currentState + ' ' + character ];
+        let character = string[0];
+        let state = this.states[this.currentState + ' ' + character];
 
-        if( state === undefined ) {
+        if (state === undefined) {
             return false;
         }
 
         this.currentState = state;
 
-        return this.run( string.substr( 1 ) );
+        return this.run(string.substr(1));
     }
 
-    fromRegularExpression( regularExpression ) {
-
-    }
+    fromRegularExpression(regularExpression) {}
 }
