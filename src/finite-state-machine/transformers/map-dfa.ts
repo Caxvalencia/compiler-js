5bc409e51d09dc1b16bdbd0293d26843beeef2e8
Caxvalencia <caxvalencia@gmail.com>
2018-03-18 00:45:02 -0500

    Fix and refactor, apply method only pushed states one time into accepts array

--- src/finite-state-machine/transformers/map-dfa.ts
+++ src/finite-state-machine/transformers/map-dfa.ts
@@ -1,41 +1,54 @@
 import { State } from '../state';
 
+/**
+ * @export
+ * @class MapDFA
+ */
 export class MapDFA {
     states: { [key: string]: number };
-    accepts: number[];
-
+    accepts: any[];
+    
+    /**
+     * Creates an instance of MapDFA.
+     * @constructor
+     */
     constructor() {
         this.states = {};
         this.accepts = [];
     }
 
     /**
-     * @param dfa Deterministic Finite Automata
+     * @static
+     * @param  {State} dfa Deterministic Finite Automata
+     * @return {MapDFA}
      */
     static apply(dfa: State) {
         return new MapDFA().apply(dfa);
     }
 
+    /**
+     * @private
+     * @param  {State} dfa
+     * @return {MapDFA}
+     */
     private apply(dfa: State) {
-        let toJson = dfa => {
-            if (dfa.isAccepted) {
-                this.accepts.push(dfa.id);
+        if (dfa.isAccepted && this.accepts.indexOf(dfa.id) === -1) {
+            this.accepts.push(dfa.id);
+        }
+
+        let transitions = dfa.getTransitions();
+
+        for (let key in transitions) {
+            const id: string = dfa.id + '-' + key;
+
+            if (this.states[id]) {
+                continue;
             }
 
-            let transitions = dfa.getTransitions();
+            this.states[id] = <number>transitions[key][0].id;
+            this.apply(transitions[key][0]);
+        }
 
-            for (let key in transitions) {
-                if (this.states[dfa.id + '-' + key]) {
-                    continue;
-                }
-
-                this.states[dfa.id + '-' + key] = transitions[key][0].id;
-                toJson(transitions[key][0]);
-            }
-
-            return this;
-        };
-
-        return toJson(dfa);
+        return this;
     }
 }
