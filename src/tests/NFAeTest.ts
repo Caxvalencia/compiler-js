1c3c193817e444a952a884905196b829e611f6cf
Caxvalencia <caxvalencia@gmail.com>
2018-03-20 13:20:50 -0500

    Add methods for test of NFAe class

--- src/tests/NFAeTest.ts
+++ src/tests/NFAeTest.ts
@@ -1,6 +1,110 @@
-import { suite } from 'mocha-typescript';
+import { assert } from 'chai';
+import { test, suite } from 'mocha-typescript';
+import { Operators } from '../finite-state-machine/constants/operators';
+import { RegularExpresion } from '../RegularExpresion';
+import { NFAe } from '../finite-state-machine/nfae';
 
 @suite('NFAeTest')
 export class NFAeTest {
-    
+    @test
+    public testSimpleNFAe() {
+        let nfae = NFAe.convert('A'.split(''));
+
+        assert.deepEqual(nfae.getAlphabet(), ['A']);
+        assert.isTrue(nfae.getFsm().process('A')[0].isAccepted, 'A founded');
+    }
+
+    @test
+    public testConcatNFAe() {
+        let nfae = NFAe.convert('AB'.split(''));
+
+        assert.deepEqual(nfae.getAlphabet(), ['A', 'B']);
+
+        assert.isTrue(
+            nfae
+                .getFsm()
+                .process('A')[0]
+                .process(Operators.EPSILON)[0]
+                .process('B')[0].isAccepted,
+            'AB founded'
+        );
+    }
+
+    @test
+    public testKleeneNFAe() {
+        let nfae = NFAe.convert('A*'.split(''));
+
+        assert.deepEqual(nfae.getAlphabet(), ['A']);
+
+        assert.isTrue(
+            nfae.getFsm().process(Operators.EPSILON)[1].isAccepted,
+            'A* - O ocurrences founded'
+        );
+
+        assert.isTrue(
+            nfae
+                .getFsm()
+                .process(Operators.EPSILON)[0]
+                .process('A')[0]
+                .process(Operators.EPSILON)[0].isAccepted,
+            'A* - 1 ocurrences founded'
+        );
+
+        assert.isTrue(
+            nfae
+                .getFsm()
+                .process(Operators.EPSILON)[0]
+                .process('A')[0]
+                .process(Operators.EPSILON)[0]
+                .process(Operators.EPSILON)[0]
+                .process('A')[0]
+                .process(Operators.EPSILON)[0].isAccepted,
+            "A* - Many 'A' ocurrences founded"
+        );
+    }
+
+    @test
+    public testUnionNFAe() {
+        let source = 'A|B';
+        let nfae = NFAe.convert(source.split(''));
+
+        assert.deepEqual(nfae.getAlphabet(), ['A', 'B']);
+
+        assert.isTrue(
+            nfae
+                .getFsm()
+                .process(Operators.EPSILON)[0]
+                .process('A')[0].isAccepted,
+            source + ' - A ocurrence founded'
+        );
+
+        assert.isTrue(
+            nfae
+                .getFsm()
+                .process(Operators.EPSILON)[1]
+                .process('B')[0].isAccepted,
+            source + ' - B ocurrence founded'
+        );
+    }
+
+    @test
+    public testUnionKleeneNFAe() {
+        let source = 'A*B*';
+        let nfae = NFAe.convert(source.split(''));
+
+        assert.deepEqual(nfae.getAlphabet(), ['A', 'B']);
+
+        assert.isTrue(
+            nfae
+                .getFsm()
+                .process(Operators.EPSILON)[0]
+                .process('A')[0]
+                .process(Operators.EPSILON)[0]
+                .process(Operators.EPSILON)[1]
+                .process(Operators.EPSILON)[0]
+                .process('B')[0]
+                .process(Operators.EPSILON)[0].isAccepted,
+            source
+        );
+    }
 }
