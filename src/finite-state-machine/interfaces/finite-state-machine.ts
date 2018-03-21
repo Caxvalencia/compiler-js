3257792e6665265e1225b5602ccc003e5cc7319c
Caxvalencia <caxvalencia@gmail.com>
2018-03-21 13:48:50 -0500

    Add FiniteStateMachine interface

---
+++ src/finite-state-machine/interfaces/finite-state-machine.ts
@@ -0,0 +1,18 @@
+import { State } from '../state';
+
+export interface IFiniteStateMachine {
+    /**
+     * @returns {IFiniteStateMachine}
+     */
+    convert(): any;
+
+    /**
+     * @returns {string[]}
+     */
+    getAlphabet(): string[];
+
+    /**
+     * @returns {State}
+     */
+    getFsm(): State;
+}
