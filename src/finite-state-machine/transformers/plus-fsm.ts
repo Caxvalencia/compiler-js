011d6d7a61af3f736632eb40e1b502079a96b16d
Caxvalencia <caxvalencia@gmail.com>
2018-03-26 00:02:27 -0500

    Add plus fsm transform class

---
+++ src/finite-state-machine/transformers/plus-fsm.ts
@@ -0,0 +1,26 @@
+import { Operators } from '../constants/operators';
+import { IFsm } from '../interfaces/fsm';
+import { State } from '../state';
+import { SimpleFsm } from './simple-fsm';
+
+export class PlusFsm {
+    /**
+     * @static
+     * @param {IFsm} fsm
+     * @returns {IFsm}
+     */
+    static apply(fsm: IFsm): IFsm {
+        let kleene = new SimpleFsm();
+        kleene.init = new State();
+        kleene.end = new State(Operators.EPSILON, [fsm.end], false);
+
+        for (let key in fsm.init.getTransitions()) {
+            kleene.init.addTransition(key, [kleene.end]);
+        }
+
+        fsm.init.setTransitions({ [Operators.EPSILON]: [kleene.init] });
+        fsm.end.setTransitions({ [Operators.EPSILON]: [kleene.init] });
+
+        return fsm;
+    }
+}
