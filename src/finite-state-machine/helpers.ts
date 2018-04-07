f6f422b469b55bbd952e35da4eb9752876a94e8b
Caxvalencia <caxvalencia@gmail.com>
2018-04-06 23:14:32 -0500

    Add helpers class

---
+++ src/finite-state-machine/helpers.ts
@@ -0,0 +1,27 @@
+import { Transition, State } from './state';
+
+export class Helpers {
+    static replaceEnd(
+        transitions: Transition,
+        endStateToFind,
+        nextState: State
+    ): Transition {
+        for (let key in transitions) {
+            if (transitions[key].indexOf(endStateToFind) === -1) {
+                transitions[key].forEach((state: State) => {
+                    Helpers.replaceEnd(
+                        state.getTransitions(),
+                        endStateToFind,
+                        nextState
+                    );
+                });
+
+                continue;
+            }
+
+            transitions[key] = [nextState];
+        }
+
+        return transitions;
+    }
+}
